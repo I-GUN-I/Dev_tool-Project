aac93d34764670063c2fb81a5a1fc9a3c6dcce5a
Suphawit <112681287+suphawit1@users.noreply.github.com>
2024-10-01 22:13:07 +0700

    Update models.py

--- safestep/report/models.py
+++ safestep/report/models.py
@@ -28,7 +28,7 @@ class FallEvent(models.Model):
     def __str__(self):
         return f"{self.user_id} - {self.timestamp}"
 
-class twilioAPI(models.Model):
+class TwilioAPI(models.Model):
     account_sid = models.CharField(max_length=40)
     auth_token = models.CharField(max_length=40)
     tel = models.CharField(max_length=20)
